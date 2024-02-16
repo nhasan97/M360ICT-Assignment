7d6ce88d5dc3395842d8b6fb8bbb38a8171bf3ce
nhasan97 <nh6069325@gmail.com>
2024-02-16 19:42:07 +0600

    restructured Navbar component using Flex of antd, added search bar which consists of antd input and button, notification button and avatar along with css styling, removed unnecessary styles and added two icon images

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,61 @@
 import React from "react";
-import { Layout } from "antd";
+import { Avatar, Button, Flex, Input, Layout } from "antd";
+import searchIcone from "../assets/loupe 1.png";
+import notificationIcone from "../assets/notification-bell 1.png";
 
 const { Header } = Layout;
 
 const headerStyle: React.CSSProperties = {
   textAlign: "center",
-  // color: "#fff",
-  height: 64,
-  paddingInline: 48,
-  lineHeight: "64px",
+  height: "fit-content",
   backgroundColor: "white",
+  padding: "23px 38px 0px 38px",
+};
+
+const inputStyle: React.CSSProperties = {
+  backgroundColor: "#F0F5FA",
+  width: "539px",
+  height: "54px",
+  fontSize: "14px",
+  fontWeight: "500",
+  borderRadius: "14px",
+  padding: "0px 20px",
+};
+
+const buttonStyle: React.CSSProperties = {
+  position: "absolute",
+  background: "transparent",
+  border: "none",
+  boxShadow: "none",
+  transform: "translate(-10px,0px)",
 };
 
 const Navbar = () => {
-  return <Header style={headerStyle}>Header</Header>;
+  return (
+    <Header style={headerStyle}>
+      <Flex justify="space-between" align="center">
+        <Flex
+          justify="flex-end"
+          align="center"
+          style={{
+            position: "relative",
+          }}
+        >
+          <Input placeholder="Search" variant="filled" style={inputStyle} />
+          <Button style={buttonStyle}>
+            <img src={searchIcone} alt="" />
+          </Button>
+        </Flex>
+
+        <Flex align="center" gap={24}>
+          <Button style={{ border: "none", boxShadow: "none" }}>
+            <img src={notificationIcone} alt="" />
+          </Button>
+          <Avatar size={47} />
+        </Flex>
+      </Flex>
+    </Header>
+  );
 };
 
 export default Navbar;
