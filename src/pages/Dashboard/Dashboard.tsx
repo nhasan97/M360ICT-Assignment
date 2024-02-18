f33c35f5a9e144739a3eaa0dec4a3bf06f3949ea
nhasan97 <nh6069325@gmail.com>
2024-02-18 23:56:53 +0600

    added users image and antd  card containing active and idle users in Dashboard.tsx component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,51 @@
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import { Card, Col, Flex, Row, Statistic } from "antd";
+import groupImg from "../../assets/group.png";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const dashboardConatinerStyle: React.CSSProperties = {
+    width: "100%",
+    height: "100%",
+    overflowY: "auto",
+  };
+
+  return (
+    <Flex
+      vertical
+      justify="center"
+      align="center"
+      style={dashboardConatinerStyle}
+    >
+      <img src={groupImg} style={{ width: "30%" }}></img>
+
+      <Row gutter={16}>
+        <Col span={12}>
+          <Card bordered={false}>
+            <Statistic
+              title="Active"
+              value={11.28}
+              precision={2}
+              valueStyle={{ color: "#3f8600" }}
+              prefix={<ArrowUpOutlined />}
+              suffix="%"
+            />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card bordered={false}>
+            <Statistic
+              title="Idle"
+              value={39.3}
+              precision={2}
+              valueStyle={{ color: "#cf1322" }}
+              prefix={<ArrowDownOutlined />}
+              suffix="%"
+            />
+          </Card>
+        </Col>
+      </Row>
+    </Flex>
+  );
 };
 
 export default Dashboard;
---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+// import { Navigate, useLocation } from "react-router-dom";
+// import Loading from "../components/Loading";
+
+const PrivateRoute = ({ children }) => {
+  //   const { user, loading } = ;
+  //   const location = useLocation();
+  //   if (loading) {
+  //     return <Loading />;
+  //   }
+  //   if (user?.email) {
+  //     return children;
+  //   }
+  //   return (
+  //     <Navigate to="/login" state={{ from: location }} replace="true"></Navigate>
+  //   );
+};
+
+export default PrivateRoute;
